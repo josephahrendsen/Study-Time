93de7fd86c8170640c22a9d57d8315ac41170088
Helquin <47200030+Helquin@users.noreply.github.com>
2019-09-28 05:11:12 -0400

    Add files via upload
    Changed how the program looked for NSFW websites. It will close them. One bug being that if it loops all the way around 
    [1,2,3,4,5,6,7] (going in descending order 7 to 1 starting at 4), it will skip over 6. Images are no longer needed. You will need to install  pyperclip in order to run! Good luck guys. I finished this at like 4:30 but kept working on it until 5:11

--- Automation.py
+++ Automation.py
@@ -1,31 +1,89 @@
-import os
 import pyautogui as pag
 import time
+import pyperclip
 
-# how long to wait
+# while loop to check each tab
 print('4')
 time.sleep(1)
 print('3')
 time.sleep(1)
 print('2')
 time.sleep(1)
-
 print('1')
-# closing the tab
-print("Locating Reddit")
+time.sleep(1)
+pag.hotkey('alt', 'tab')
+#finding out first tab that isn't NSFW
+firstUrl =""
+a = 0
+while a == 0:
+
+
+    pag.hotkey('ctrl', 'e')
+    time.sleep(.2)
+    pag.hotkey('ctrl', 'z')
+    time.sleep(.2)
+    pag.hotkey('ctrl', 'a')
+    time.sleep(.2)
+    pag.hotkey('ctrl', 'c')
+
+    firstUrl = pyperclip.paste()
+    print(firstUrl)
+    if "facebook.com" in firstUrl or"youtube" in firstUrl or "twitter" in firstUrl or "reddit" in firstUrl or "imgur" in firstUrl or "tumblr" in firstUrl or "coolmathgames" in firstUrl or "poptropica" in firstUrl:
+        pag.hotkey("ctrl","w")
+
+    else:
+        firstUrl = pyperclip.paste()
+        print(firstUrl)
+        a = 1
 
 
 
-# moving to reddit
-print("Mouse Position:", pag.position())
 
-time.sleep(3)
-a = pag.locateCenterOnScreen(r"C:\Users\Helquin\PycharmProjects\GoogleCloudHackAthon\Reddit1.PNG")
+urlList = [1]
+i = 0
+while firstUrl != urlList[i]:
+    # move up the tabs
+    #Debug
+    #print(firstUrl)
+    pag.hotkey('ctrl', 'pageup')
 
-print("we found reddit at", a)
+    # takes the URL of that Tab
+    pag.hotkey('ctrl', 'e')
+    time.sleep(.4)
+    pag.hotkey('ctrl', 'z')
+    time.sleep(.4)
+    pag.hotkey('ctrl', 'a')
+    time.sleep(.4)
+    pag.hotkey('ctrl', 'c')
+    urlList.append(pyperclip.paste())
+    compare = str(urlList[i+1])
+
+    #for Debugging
+    #print(compare)
 
 
-#pag.hotkey('ctrl', 'w')
+    # check if it is one of our NSFW sites
+    if "facebook" in compare:
+        pag.hotkey("ctrl","w")
+    if "youtube" in compare:
+        pag.hotkey("ctrl","w")
+    if "tumblr" in compare:
+        pag.hotkey("ctrl","w")
+    if "reddit" in compare:
+        pag.hotkey("ctrl","w")
+    if "coolmathgames" in compare:
+        pag.hotkey("ctrl","w")
+    if "imgur" in compare:
+        pag.hotkey("ctrl","w")
+    if "poptropica" in compare:
+        pag.hotkey("ctrl","w")
+    if "twitter" in compare:
+        pag.hotkey("ctrl","w")
 
 
 
+
+    i = i + 1
+
+
+print (urlList)
