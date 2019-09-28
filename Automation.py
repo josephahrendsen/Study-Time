c59a27df3ccfe12fe4982f03a041345c6139d99e
Helquin <47200030+Helquin@users.noreply.github.com>
2019-09-28 01:41:39 -0400

    Add files via upload
    The Png used for the py file and the py file

---
+++ Automation.py
@@ -0,0 +1,32 @@
+import os
+import pyautogui as pag
+import time
+
+# how long to wait
+print('4')
+time.sleep(1)
+print('3')
+time.sleep(1)
+print('2')
+time.sleep(1)
+
+print('1')
+# closing the tab
+print("Locating Reddit")
+
+
+
+# moving to reddit
+print("Mouse Position:", pag.position())
+
+pag.moveTo(61,74)
+time.sleep(3)
+a = pag.locateCenterOnScreen(r"C:\Users\Helquin\PycharmProjects\GoogleCloudHackAthon\Reddit1.PNG")
+
+print("we found reddit at", a)
+
+
+#pag.hotkey('ctrl', 'w')
+
+
+
